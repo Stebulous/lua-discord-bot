500076389f259c7ab6e7193b35b773f8fde3402f
Dosage <sky34416@gmail.com>
2022-06-20 12:38:20 -0400

    Delete config.lua

--- src/config.lua
+++
@@ -1,7 +0,0 @@
-local config = {
-    token = 'OTg4MzI0OTE5OTIwMTg5NDQw.GcfrqG.PFmJ8Kx5cfQXBUmEejc7QZ-GCMImSxVzRjJnaM',
-    prefix = '!',
-    developerid = '847655894745350144',
-}
-
-return config
